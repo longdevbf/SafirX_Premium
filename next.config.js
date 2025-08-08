c1ddbed556d91d28c2fba03f3e7c5106c30b7734
Duc Long <longspeed001@gmail.com>
2025-08-08 16:22:10 +0700

    Fix Next.js images - add IPFS gateway support

--- next.config.js
+++ next.config.js
@@ -9,6 +9,36 @@ module.exports = {
     // your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
+  // Allow external images from IPFS gateways
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gateway.pinata.cloud',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'jade-rainy-parrotfish-621.mypinata.cloud',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.ipfs.dweb.link',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ipfs.io',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+    unoptimized: true, // Disable optimization for IPFS images
+  },
   // Exclude contract folder from build
   experimental: {
     externalDir: true,
