4481ddd343b08528212fbb1e558240be65081bdc
Duc Long <longspeed001@gmail.com>
2025-08-10 20:23:39 +0700

    fix wallet

--- src/components/config/wagmiConfig.ts
+++ src/components/config/wagmiConfig.ts
@@ -1,28 +1,14 @@
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-import { metaMaskWallet, phantomWallet } from '@rainbow-me/rainbowkit/wallets';
-import { http } from 'wagmi';
-import { sapphire, sapphireTestnet } from 'wagmi/chains';
-import { createSapphireConfig } from '@oasisprotocol/sapphire-wagmi-v2';
 
-// Create connectors with only MetaMask
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommend',
-      wallets: [metaMaskWallet],
-    },
-  ],
-  {
-    appName: 'SafirX Premium',
-    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '12345',
-  }
-);
+import { http } from 'wagmi';
+import { sapphireTestnet } from 'wagmi/chains';
+import { createSapphireConfig , sapphireHttpTransport} from '@oasisprotocol/sapphire-wagmi-v2';
 
 export const config = createSapphireConfig({
-  sapphireConfig: {},
-  connectors,
+  sapphireConfig: {
+    replaceProviders: true,
+  },
   chains: [sapphireTestnet],
   transports: {
-    [sapphireTestnet.id]: http(),
+    [sapphireTestnet.id]: sapphireHttpTransport(),
   },
 });
